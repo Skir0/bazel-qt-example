e3b61b242f82f40f1892245ee6a2732ad910b814
Skiro <skiro6600200@gmail.com>
2025-04-06 10:59:53 +0300

    test

---
+++ labs/graphics/Ray.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Kirill Smychok on 6.04.25.
+//
+
+#include "Ray.h"
---
+++ labs/graphics/Ray.h
@@ -0,0 +1,16 @@
+//
+// Created by Kirill Smychok on 6.04.25.
+//
+
+#ifndef RAY_H
+#define RAY_H
+
+
+
+class Ray {
+
+};
+
+
+
+#endif //RAY_H
